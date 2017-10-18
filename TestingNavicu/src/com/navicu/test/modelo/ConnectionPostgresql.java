6f0305dbf19e19118b39edf5653d9bf273317b3b
devlopervit-05 <devlopervit-05@devlopervit05>
2017-10-18 11:13:31 -0400

    % Creando ventana login

--- TestingNavicu/src/com/navicu/test/modelo/ConnectionPostgresql.java
+++ TestingNavicu/src/com/navicu/test/modelo/ConnectionPostgresql.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.navicu.test.controlador;
+package com.navicu.test.modelo;
 
 /**
  *
