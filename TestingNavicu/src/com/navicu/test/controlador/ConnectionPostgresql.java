8669a2ab5a408c9c562903f8a291af2e341b1260
devlopervit-05 <devlopervit-05@devlopervit05>
2017-10-18 09:24:47 -0400

    % Creando clase modelo

---
+++ TestingNavicu/src/com/navicu/test/controlador/ConnectionPostgresql.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.navicu.test.controlador;
+
+/**
+ *
+ * @author devlopervit-05
+ */
+public class ConnectionPostgresql {
+    
+}
