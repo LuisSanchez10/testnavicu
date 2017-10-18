c370972dba178c0ac14a1ad8daf6ece1215464a9
devlopervit-05 <devlopervit-05@devlopervit05>
2017-10-18 16:52:19 -0400

    % Completando modelo de conexión DB

--- TestingNavicu/src/com/navicu/test/modelo/ConnectionPostgresql.java
+++ TestingNavicu/src/com/navicu/test/modelo/ConnectionPostgresql.java
@@ -4,11 +4,24 @@
  * and open the template in the editor.
  */
 package com.navicu.test.modelo;
-
+import java.sql.*;
 /**
  *
  * @author devlopervit-05
  */
 public class ConnectionPostgresql {
+    Connection connect = null;
     
+    public Connection Connectionsql(){
+        try {
+              Class.forName("org.postgresql.Driver");
+              connect = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/testnavicu","qajhbulp9q9ohj", "02805dd8226b356e1c1547c1bd313qansd");
+           } catch (Exception e) {
+              e.printStackTrace();
+              System.err.println(e.getClass().getName()+": "+e.getMessage());
+              System.exit(0);
+           } 
+   return connect;
 }
+}
+
